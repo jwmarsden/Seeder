0417968d0b24c085834598d0517350bab5987e90
openecho <j.w.marsden@gmail.com>
2010-05-25 22:55:45 +1000

    Created Profile Domain Class and Tests

---
+++ grails-app/domain/openecho/Profile.groovy
@@ -0,0 +1,44 @@
+package openecho
+
+class Profile {
+
+  static hasMany = [ users : User ] //, followerProfiles : FollowProfile, profilesFollowing : FollowProfile, seeds : Seed ]
+//  static mappedBy = [ followerProfiles : "following", profilesFollowing : "follower"]
+//  static fetchMode = [ followerProfiles : "eager", profilesFollowing : "eager"]
+
+  String identity
+  String firstName
+  String lastName
+  String displayName
+  Date birthDate
+  String biography
+  String homepage
+  String email
+  String timezone
+  String city
+  String country
+  byte[] photo
+
+  Date dateCreated
+  Date lastUpdated
+
+  static constraints = {
+    identity(nullable: false, unique: true, maxSize: 150)
+    firstName(nullable: true)
+    lastName(nullable: true)
+    displayName(nullable: true, maxSize: 150)
+    birthDate(nullable: true)
+    biography(nullable: true, maxSize: 1000)
+    homepage(url: true, nullable: true)
+    email(email: true, nullable: true)
+    timezone(nullable: true)
+    city(nullable: true)
+    country(nullable: true)
+    photo(nullable: true)
+  }
+
+  String toString() {
+    "Profile for ${identity} (${id})"
+  }
+
+}
---
+++ test/unit/openecho/ProfileTests.groovy
@@ -0,0 +1,61 @@
+package openecho
+
+import grails.test.*
+
+class ProfileTests extends GrailsUnitTestCase {
+  protected void setUp() {
+    super.setUp()
+  }
+
+  protected void tearDown() {
+    super.tearDown()
+  }
+
+  void testCreateNewProfile() {
+    mockDomain(Profile)
+    def emptyProfile = new Profile()
+    assertNull(emptyProfile.save())
+    def profile = new Profile(identity: "openecho")
+    assertNotNull(profile.save())
+  }
+
+  void testCreateNewProfileWithUser() {
+    mockDomain(Profile)
+    mockDomain(User)
+    def user = new User(userName: 'jwm', newPassword: 'mwjwm', type: "LOCAL", email: 'jwm@gmail.com')
+    assertNotNull(user.save())
+    def profile = new Profile(identity: "openecho")
+    assertNotNull(profile.save())
+    profile.addToUsers(user);
+    assertNotNull(profile.save())
+  }
+
+  void testCreateNewProfileWithMultiUsers() {
+    mockDomain(Profile)
+    mockDomain(User)
+    // Create some users
+    def user1 = new User(userName: 'jwm', newPassword: 'mwjwm', type: "LOCAL", email: 'jwm@gmail.com')
+    assertNotNull(user1.save())
+    def user2 = new User(userName: 'mwj', newPassword: 'mwjwm', type: "LOCAL", email: 'mwj@gmail.com')
+    assertNotNull(user1.save())
+    // Create Profile
+    def profile = new Profile(identity: "seeder org")
+    assertNotNull(profile.save())
+    // Add users to profile
+    user1.addToProfiles(profile)
+    assertNotNull(user1.save())
+    user2.addToProfiles(profile)
+    assertNotNull(user2.save())
+    assertEquals(profile.users.size(), 2)
+    assertEquals(user1.profiles.size(), 1)
+    assertEquals(user2.profiles.size(), 1)
+    // Create another User
+    def user3 = new User(userName: 'pre', newPassword: 'prepre', type: "LOCAL", email: 'pre@gmail.com')
+    assertNotNull(user3.save())
+    // Add profile to User
+    profile.addToUsers(user3)
+    assertNotNull(profile.save())
+    assertEquals(profile.users.size(), 3)
+    assertEquals(user3.profiles.size(), 1)
+  }
+}
