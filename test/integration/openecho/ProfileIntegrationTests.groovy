1c70759d014d7c560aa5f2d52104d7fa9d814d63
openecho <j.w.marsden@gmail.com>
2010-05-26 13:48:33 +1000

    Updated Following Tests

--- test/integration/openecho/ProfileIntegrationTests.groovy
+++ test/integration/openecho/ProfileIntegrationTests.groovy
@@ -54,7 +54,7 @@ class ProfileIntegrationTests extends GrailsUnitTestCase {
     assertEquals(user3.profiles.size(), 1)
   }
 
-  void testFollowProfile() {
+  void testFollowProfileCreateFollowProfile() {
     println "= creating profile1"
     def profile1 = new Profile(identity: "openecho")
     assertNotNull(profile1.save(flush: true))
@@ -84,5 +84,50 @@ class ProfileIntegrationTests extends GrailsUnitTestCase {
     assertEquals(0, profile1.followerProfiles.size())
     assertEquals(0, profile2.profilesFollowing.size())
     assertEquals(1, profile2.followerProfiles.size())
+  }
+
+  void testFollowProfileInjectionVariable() {
+    println "= profile1 follow profile2"
+    def profile1 = new Profile(identity: "openecho")
+    assertNotNull(profile1.save(flush: true))
+    def profile2 = new Profile(identity: "seeder org")
+    assertNotNull(profile2.save(flush: true))
+    profile1.addToProfilesFollowing(follower: profile1, following: profile2)
+    assertNotNull(profile1.save(flush: true))
+    profile1.refresh()
+    profile2.refresh()
+    println "= profile1: \n\tFollowing {$profile1.profilesFollowing} Followers: {$profile1.followerProfiles}"
+    println "= profile2: \n\tFollowing {$profile2.profilesFollowing} Followers: {$profile2.followerProfiles}"
+    assertEquals(1, profile1.profilesFollowing.size())
+    assertEquals(0, profile1.followerProfiles.size())
+    assertEquals(0, profile2.profilesFollowing.size())
+    assertEquals(1, profile2.followerProfiles.size())
+    println "= profile2 follow profile1"
+    profile2.addToProfilesFollowing(follower: profile2, following: profile1)
+    assertNotNull(profile2.save(flush: true))
+    profile1.refresh()
+    profile2.refresh()
+    println "= profile1: \n\tFollowing {$profile1.profilesFollowing} Followers: {$profile1.followerProfiles}"
+    println "= profile2: \n\tFollowing {$profile2.profilesFollowing} Followers: {$profile2.followerProfiles}"
+    assertEquals(1, profile1.profilesFollowing.size())
+    assertEquals(1, profile1.followerProfiles.size())
+    assertEquals(1, profile2.profilesFollowing.size())
+    assertEquals(1, profile2.followerProfiles.size())
+    println "= profile3 followed by profile2"
+    def profile3 = new Profile(identity: "sammy")
+    profile3.addToFollowerProfiles(follower: profile2, following: profile3)
+    assertNotNull(profile3.save(flush: true))
+    profile1.refresh()
+    profile2.refresh()
+    profile3.refresh()
+    println "= profile1: \n\tFollowing {$profile1.profilesFollowing} Followers: {$profile1.followerProfiles}"
+    println "= profile2: \n\tFollowing {$profile2.profilesFollowing} Followers: {$profile2.followerProfiles}"
+    println "= profile3: \n\tFollowing {$profile3.profilesFollowing} Followers: {$profile3.followerProfiles}"
+    assertEquals(1, profile1.profilesFollowing.size())
+    assertEquals(1, profile1.followerProfiles.size())
+    assertEquals(2, profile2.profilesFollowing.size())
+    assertEquals(1, profile2.followerProfiles.size())
+    assertEquals(0, profile3.profilesFollowing.size())
+    assertEquals(1, profile3.followerProfiles.size())
   }
 }
